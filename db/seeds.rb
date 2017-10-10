c7748ef0a4205ab8700f2f9f3e480db832bd6258
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-10 16:25:12 -0400

    added default values for our databse and ran rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,26 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# ======================================================================
+
+# Again, all we've done is write some instructions about how to create three pictures, but they won't be added to our database until we run the code in this file. We can run the file with the following command: $ rails db:seed
+
+Picture.create(
+  title:  'Sea Power',
+  artist: 'Stephen Scullion',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg'
+)
+
+Picture.create(
+  title:  'Into the Poppies',
+  artist: 'John Wilhelm',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg'
+)
+
+Picture.create(
+  title:  'Sea Power',
+  artist: 'Stephen Scullion',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg'
+)
