c931e946dcabac9745a9ddbd560b34ca4aeed95e
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-10 17:30:08 -0400

    added the dynamic route for each picture using :id that is stored in params and then show that particular picture when we call the specific id#

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,6 +4,11 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def show
+    # We can use ActiveRecord's find method and the :id portion of the URL from the params hash in order to do this.
+    @picture = Picture.find(params[:id])
+  end
+
 end
 
 # Running the following command will tell Rails to create this controller class and put it in the right place: rails generate controller pictures
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # --------GET REQUESTS -----------
   get 'pictures' => 'pictures#index'
   # looks for idex in pictures folder of views when /pictures is called by the get request.
+
+  # -------- DYNAMIC ROUTES ------------
+  # Whatever value takes the place of :id in the URL will be stored in the params hash under the key :id
+  get 'pictures/:id' => 'pictures#show'
+
+
+
+
+
 end
